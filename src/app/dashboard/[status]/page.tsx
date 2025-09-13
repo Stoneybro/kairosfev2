bc68687a9aed9aaa5d7139e83f73ab44667cca65
Stoneybro <zionlivingstone4@gmail.com>
2025-09-13 09:59:34 +0100

    refactor: simplify Page component by removing unused parameters

--- src/app/dashboard/[status]/page.tsx
+++ src/app/dashboard/[status]/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
-export default function Page({ params }: { params: { status: string } }) {
+export default function Page() {
   redirect("/dashboard");
 }
