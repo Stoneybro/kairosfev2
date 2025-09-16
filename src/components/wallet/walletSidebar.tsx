71f4031c62a2fb2059f0c10bcb448ca95f1df96b
Stoneybro <zionlivingstone4@gmail.com>
2025-09-16 01:47:04 +0100

    bug: removed wallet sidebar build error

--- src/components/wallet/walletSidebar.tsx
+++ src/components/wallet/walletSidebar.tsx
@@ -7,7 +7,6 @@ import WalletFooter from "./walletFooter";
 import { activeTabType } from "@/types";
 import WalletRecieve from "./walletReceive";
 import WalletSend from "./walletSend";
-import WalletActivity from "./walletActivity";
 import WalletSettings from "./walletSettings";
 import Activity from "./walletActivity/activity";
 import WalletFaucet from "./walletFaucet";
